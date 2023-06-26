5074cdbe19612fe331004d3047642990a0320537
Uthirapathi <silaluthirapathi@gmail.com>
2023-06-26 16:32:20 +0530

    Initial commit

---
+++ src/test/java/com/git/features/demo/GitFeaturesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.features.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFeaturesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
